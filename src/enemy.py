abcbee8e1c9f49aed141bf8d14a2af5092d7640d
declipz <declipz1121@gmail.com>
2018-11-27 11:52:28 +0300

    Spacehunters v.0.1.0 improved graphics and game engine

--- src/enemy.py
+++ src/enemy.py
@@ -1,9 +1,14 @@
+import pygame
+
 from src.character import Character
 from src.drawable import Drawable
 from src.bullet import Bullet
 from src.config import config
 
 class Enemy(Character):
+
+    enemyTile = pygame.image.load("enemy.png")
+
     def __init__(self, game, 
                 x, 
                 y, 
@@ -19,21 +24,27 @@ class Enemy(Character):
                                 width, height, color, 
                                 velocity, healthPoints, 
                                 damage, bulletsPerShot)
+        self.enemyTile = pygame.transform.scale(self.enemyTile, (self.width, self.height))
         self.target = target
-        self.shootRateTick = 30
+        self.shootRateTick = 0
 
     def shoot(self):
         if self.shootRateTick > 0:
             self.shootRateTick -= 1
             return
-        bullet = Bullet(self.game, self.centerX, self.borderBottom + 10, self.target.centerX, self.target.centerY, 100, owner=self)
+        bullet = Bullet(self.game, self.centerX, self.borderBottom + 10, self.centerX, config["game"]["height"], 100, owner=self, color=config["enemy"]["bulletColor"])
         self.game.gameObjects.append(bullet)
         self.shootRateTick = 120
 
     def draw(self):
-        Drawable.draw(self)
+        self.game.display.blit(self.enemyTile, (self.x, self.y))
+
+    def update(self):
+        if self.healthPoints <= 0:
+            self.die()
+        self.draw()
         self.shoot()
-        self.move(0, config["enemy"]["velocity"])
+        self.move(0, config["enemy"]["velocity"])        
     
     def move(self, deltaX, deltaY):
         Character.move(self, deltaX, deltaY)
